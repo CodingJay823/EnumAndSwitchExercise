a0d976a45ad39f11b8d734dda7a91acf9f0709f4
CodingJay823 <67976024+CodingJay823@users.noreply.github.com>
2020-07-22 15:47:32 -0500

    Fixed it so that it has more options.

--- Jada_H_EnumSwitchExercise.playground/Contents.swift
+++ Jada_H_EnumSwitchExercise.playground/Contents.swift
@@ -5,22 +5,34 @@ import UIKit
 // Make enums for all of the ingredients
 enum Breadtype {
     case french
+    case white
+    case wheat
 }
 
 enum MeatType {
     case turkey
+    case ham
+    case salami
+    case bacon
 }
 
 enum CheeseType {
     case provolone
+    case mazzarella
+    case american
+    case cheddar
 }
 
 enum Veggies {
     case lettuce
+    case tomato
+    case onion
 }
 
 enum CondimentType {
     case mayo
+    case oil
+    case pickles
 }
 
 // Create variables for the sandwhich making
@@ -28,31 +40,59 @@ var bread = Breadtype.french
 var meat = MeatType.turkey
 var cheese = CheeseType.provolone
 var veggies = Veggies.lettuce
-var condiment = CondimentType.mayo
+var condiment = CondimentType.pickles
 
 
 // Create switch statement to make the sandwhich
 switch bread {
 case .french:
-    print("French bread.")
+    print("You chose French bread as your bread.")
+case .white:
+    print("You chose White bread as your bread.")
+case .wheat:
+    print("You chose Wheat bread as your bread.")
 }
 
 switch meat {
 case .turkey:
-    print("Turkey.")
+    print("Now choose your meat.")
+    print("You picked Turkey.")
+case .bacon:
+    print("Now choose your meat.")
+    print("You chose Bacon.")
+case .ham:
+    print("Now choose your meat.")
+    print("You picked Ham.")
+case .salami:
+    print("Now choose your meat.")
+    print("You picked Salami. Not the best type but you do you.")
 }
 
 switch cheese {
 case .provolone:
-    print("Provolone cheese.")
+    print("You picked Provolone cheese, the superior cheese.")
+case .american:
+    print("You chose American cheese because you have no taste.")
+case .cheddar:
+    print("Yo decided to go with cheddar cheese Cheddar cheese because you are basic.")
+case .mazzarella:
+    print("You got Mazarella cheese ,so you have taste.")
 }
 
 switch veggies {
 case .lettuce:
-    print("Lettuce.")
+    print("You got Lettuce, so you are basic but you are the good type of basic.")
+case .tomato:
+    print("You ended up choosing Tomatoes and honestly I have nothing to say to you.")
+case .onion:
+    print("Onions, BLEGH!")
 }
 
 switch condiment {
 case .mayo:
-    print("Mayo.")
+    print("You got Mayo. Nice.")
+case .oil:
+    print("You chose Oil, but it has no taste.")
+case .pickles:
+    print("PICKLES!!!")
 }
